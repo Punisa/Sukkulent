5cba06e28b641ca875cae466bbc6c9af5e388db7
Dasha <dasha888999mail.ru>
2018-09-28 17:02:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+﻿let name = "Рамиль";
+
+switch(name) {
+
+   case 'Рамиль': {
+      if (name.length >= 6) {
+         for(let i = 0; i < 5; i++) {
+         console.log("ЯЖПРОГРАММИСТ!!");
+         }
+      }
+      break;  
+   }
+   case 'Даша': {
+      let sukkulent = {
+         name: "Евгеша",
+         color: "розовый"
+      }
+      for(let i = 0; i < 15; i++) {
+         console.log("СУККУЛЕНТЫЯЛЮЮЮЮЮБЛЮЮЮЮ!!!!! BJJJJJDDDDDDDDDD!!!!");
+      }
+      break;
+   }
+   case "Рома": {
+
+      break;
+   }
+   default: {
+      console.log('я его не знаю! чи шо?');
+      break;
+   }
+}
